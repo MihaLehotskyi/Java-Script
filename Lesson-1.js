e82e81263fb624d006b6ae230f7f1800ad0f60f7
MihaLehotskyi <78600268+MihaLehotskyi@users.noreply.github.com>
2021-07-05 21:49:44 +0300

    Add files via upload

---
+++ Lesson-1.js
@@ -0,0 +1,19 @@
+// Task 1
+let admin = 'John';
+let name = 'John';
+console.log(name);
+
+// Task 2
+let login = 'Admin';
+let password = 'TheMaster';
+if(login = 'Admin') {
+    console.log('Password?');
+    if(password = 'TheMaster'){
+        console.log('You are welcome!');
+    }else{
+        console.log('Wrong password!');
+    }
+}
+else {
+    console.log('Wrong Username!')
+}
